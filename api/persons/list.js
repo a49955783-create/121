731df921da1ef7ce29d63afc34ead459add9a2c3
a49955783-create <a49955783@gmail.com>
2025-12-18 15:57:52 +0300

    Update list.js

--- api/persons/list.js
+++ api/persons/list.js
@@ -1,10 +1,13 @@
-import {redis} from '../_redis.js';
-export default async(req,res)=>{
- const ids=await redis.smembers('persons')||[];
- const out=[];
- for(const id of ids){
-  const p=await redis.get(`person:${id}`);
-  if(p) out.push(p);
- }
- res.json(out);
-};
+import { redis } from "../_redis.js";
+
+export default async function handler(req, res) {
+  const ids = await redis.smembers("persons") || [];
+  const list = [];
+
+  for (const id of ids) {
+    const person = await redis.get(`person:${id}`);
+    if (person) list.push(person);
+  }
+
+  res.json(list);
+}
