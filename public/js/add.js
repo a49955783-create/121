dd11e9cf45a2eefb72bca7ac800504d8a0ba9ca9
a49955783-create <a49955783@gmail.com>
2025-12-18 16:01:19 +0300

    Update add.js

--- public/js/add.js
+++ public/js/add.js
@@ -1,12 +1,31 @@
-async function save(){
- const body={
-  name:name.value,nationalId:nationalId.value,age:age.value,
-  mic:mic.value,hours:hours.value,notes:notes.value,
-  grades:[{value:1},{value:1},{value:1},{value:1},{value:1},{value:2},{value:1}]
- };
- await fetch('/api/persons/create',{
-  method:'POST',headers:{'Content-Type':'application/json','x-user':'user'},
-  body:JSON.stringify(body)
- });
- alert('تم');location.href='/';
+async function save() {
+  const body = {
+    name: name.value,
+    nationalId: nationalId.value,
+    age: age.value,
+    mic: mic.value,
+    hours: hours.value,
+    notes: notes.value,
+    grades: [
+      { value: 1 },
+      { value: 1 },
+      { value: 1 },
+      { value: 1 },
+      { value: 1 },
+      { value: 2 },
+      { value: 1 }
+    ]
+  };
+
+  await fetch("/api/persons/create", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "x-user": "user"
+    },
+    body: JSON.stringify(body)
+  });
+
+  alert("تم الحفظ");
+  location.href = "/";
 }
