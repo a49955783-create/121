cf4938644f0098de90e493af731de3596ea54000
a49955783-create <a49955783@gmail.com>
2025-12-18 15:59:43 +0300

    Update index.js

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,13 @@
-fetch('/api/persons/list')
-.then(r=>r.json()).then(d=>{
- list.innerHTML='';
- d.forEach(p=>{
-  list.innerHTML+=`<div class='card'><b>${p.name}</b><br>المجموع:${p.total}</div>`;
- });
-});
+fetch("/api/persons/list")
+  .then(res => res.json())
+  .then(data => {
+    list.innerHTML = "";
+    data.forEach(p => {
+      list.innerHTML += `
+        <div class="card">
+          <b>${p.name}</b><br>
+          المجموع: ${p.total}
+        </div>
+      `;
+    });
+  });
